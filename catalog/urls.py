315aaff96bcc3f0d944abe69c3ea3e10ae1e0abd
Kvashin Valeriy <val_kv@mail.ru>
2024-04-23 23:44:19 +0200

    edit admin and urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
     path('contact/', views.ContactView.as_view(), name='contact'),
+    path('admin/', admin.site.urls),
 ]
